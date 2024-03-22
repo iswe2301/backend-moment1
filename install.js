757c4e8c7d67a8069e966e82d464ba2652b35b74
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-22 13:06:06 +0100

    Skapat en tabell till databasen, kommenterat ut koden.

--- install.js
+++ install.js
@@ -23,3 +23,21 @@ connection.query("CREATE DATABASE courses;", (error, results) => {
     if(error) throw error;
     console.log("Databas skapad");
 }); */
+
+/* Skapar en drop-tabell till databasen med SQL-fråga
+connection.query("DROP TABLE IF EXISTS Course;", (error, results) => {
+    if (error) throw error;
+    console.log("Tabellen Course raderad.");
+}); */
+
+/* Skapar en tabell till databasen med SQL-fråga
+connection.query(`CREATE TABLE Course (
+    CourseID    INT AUTO_INCREMENT PRIMARY KEY,
+    CourseCode  VARCHAR(15) NOT NULL,
+    CourseName  VARCHAR(150) NOT NULL,
+    Syllabus    VARCHAR(255) NOT NULL,
+    Progression VARCHAR(2) NOT NULL,
+    CreateDate  DATETIME DEFAULT CURRENT_TIMESTAMP)`, (error, results) => {
+    if (error) throw error;
+    console.log("Tabellen Course skapad.");
+}); */
