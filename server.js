a7623e9b2d3826701cc77facc4b00cbd993d5138
Isa Westling <isawestling.blogg@hotmail.com>
2024-03-22 13:20:34 +0100

    Inkluderat mysql och databasanslutning i server-filen, lagt till urlencoded för formulärdata, ändrat routes till async.

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Inkluderar msql-paketet
+const mysql = require("mysql");
+
 // Inkluderar express
 const express = require("express");
 const app = express(); // Startar applikationen med express
@@ -9,16 +12,36 @@ app.set("view engine", "ejs");
 // Statiska filer i katalogen public
 app.use(express.static("public"));
 
+// Inkluderar urlencoded för att kunna läsa in formulärdata
+app.use(express.urlencoded({ extended: true }));
+
+// Anslutningsinställningar
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "courses",
+    password: "mysqlpassword",
+    database: "courses"
+});
+
+// Kontrollerar errors vid anslutning
+connection.connect((error) => {
+    if (error) {
+        console.error("Anslutning misslyckades: " + error); // Skriver ut felmeddelande
+        return;
+    }
+    console.log("Ansluten till MySQL!") // Skriver ut success-meddelande vid lyckad anslutning
+});
+
 // Route för att dirigera besökare till en sida
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     res.render("index"); // Renderar startsidan
 });
 
-app.get("/addcourse", (req, res) => {
+app.get("/addcourse", async (req, res) => {
     res.render("addcourse"); // Renderar undersida
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", async (req, res) => {
     res.render("about"); // Renderar undersida
 });
 
